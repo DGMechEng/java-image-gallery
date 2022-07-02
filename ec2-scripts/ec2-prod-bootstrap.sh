2767f7e643d2dd34e37de7b62cf92e99785a2dac
DGMechEng <97802069+DGMechEng@users.noreply.github.com>
2022-07-02 10:25:15 -0600

    Update ec2-prod-bootstrap.sh
    Updated to refer to proper prod script ec2-prod-latest-m6.sh

--- ec2-scripts/ec2-prod-bootstrap.sh
+++ ec2-scripts/ec2-prod-bootstrap.sh
@@ -3,5 +3,5 @@
 #This script goes into user data for new instance
 
 export IMAGE_GALLERY_BOOTSTRAP_VERSION="1.0"
-aws s3 cp s3://edu.au.cc.image-gallery-configure/ec2-prod-latest.sh ./
-/usr/bin/bash ec2-prod-latest.sh
+aws s3 cp s3://edu.au.cc.image-gallery-configure/ec2-prod-latest-m6.sh ./
+/usr/bin/bash ec2-prod-latest-m6.sh
